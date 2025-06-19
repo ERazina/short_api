9fec832143e7fc366f3e5a8f9229e9e059e28f26
Elina Razina <e.razina@samolet.ru>
2025-06-19 23:28:34 +0700

    request succesful

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,21 +1,97 @@
-// import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./styles.scss";
 
-function App() {
+type FormValues = {
+  originalUrl: string;
+  expiresAt?: Date;
+  alias?: string;
+};
 
-    // const [alias, getAlias] = useState();
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+  fetch("http://localhost:4000/api/shorten", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Server error");
+      }
+      return response.json();
+    })
+    .then((result) => {
+      console.log("Success:", result);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  };
 
   return (
     <>
-      <form>
-        <label></label>
-        <input type="text" />
-        <label></label>
-        <input type="text" />
-        <label></label>
-        <input type="text" />
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="link" className="label">
+          Link <span style={{ color: "red" }}>*</span>
+        </label>
+        <input
+          {...register("originalUrl", {
+            required: "Link is required",
+            pattern: {
+              value: /^[A-Za-z]+$/,
+              message: "Only Latin letters allowed",
+            },
+          })}
+          placeholder="Link"
+          className={`input ${errors.originalUrl ? "input-error" : ""}`}
+        />
+        {errors.originalUrl && (
+          <p className="error">{errors.originalUrl.message}</p>
+        )}
+
+        <label htmlFor="expiresAt" className="label">
+          Link epiration date (required)
+        </label>
+        <input
+          {...register("expiresAt")}
+          placeholder="Expiration date"
+          className="input"
+        />
+        {errors.expiresAt && (
+          <p className="error">{errors.expiresAt.message}</p>
+        )}
+
+        <label htmlFor="alias" className="label">
+          Alias (optional)
+        </label>
+        <input
+          {...register("alias", {
+            pattern: {
+              value: /^[A-Za-z]+$/,
+              message: "Only Latin letters allowed",
+            },
+            maxLength: {
+              value: 20,
+              message: "Maximum length is 20 characters",
+            },
+          })}
+          placeholder="alias"
+          className="input"
+        />
+        {errors.alias && <p className="error">{errors.alias.message}</p>}
+
+        <input type="submit" className="btn" />
       </form>
     </>
   );
 }
 
-export default App;
+export default Form;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,55 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from server!");
+const links: Record<string, { originalUrl: string; expiresAt?: Date }> = {};
+
+app.post("/api/shorten", async (req: any, res: any) => {
+  const { originalUrl, expiresAt, alias } = req.body;
+
+  if (!originalUrl) {
+    return res.status(400).json({ error: "originalUrl is required" });
+  }
+
+  const finalAlias = alias || Math.random().toString(36).substring(2, 8);
+
+  if (links[finalAlias]) {
+    return res.status(409).json({ error: "Alias already exists" });
+  }
+
+  links[finalAlias] = {
+    originalUrl,
+    expiresAt: expiresAt ? new Date(expiresAt) : undefined,
+  };
+
+  return res.status(201).json({
+    message: "Short link created",
+    shortUrl: `http://localhost:3000/${finalAlias}`,
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/:alias", (req: any, res: any) => {
+  const { alias } = req.params;
+  const link = links[alias];
+
+  if (!link) {
+    return res.status(404).json({ error: "Alias not found" });
+  }
+
+  if (link.expiresAt && new Date() > link.expiresAt) {
+    return res.status(410).json({ error: "Link expired" });
+  }
+
+  return res.redirect(link.originalUrl);
+});
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
 });
